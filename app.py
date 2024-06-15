094113730ae739c36a4ce362d66db6f3fe1083f1
Badal Prasad Singh <badalstar1806@gmail.com>
2024-06-16 00:37:31 +0530

    updated requirements.txt

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-st.build_command(["pip", "install", "-U", "langchain-community"])
+
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
